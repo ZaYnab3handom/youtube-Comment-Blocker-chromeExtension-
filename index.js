d720088745c096f2b943f8a00144f9ebf78a3fac
ZaYnab <zaynab3ibrahim@gmail.com>
2023-01-29 19:54:03 +0200

    manifest & js update

---
+++ index.js
@@ -0,0 +1,12 @@
+//alert("working")
+function removeComment(){
+    var x = document.getElementById('comments')
+    console.log(x)
+    x.remove();
+ }
+// window.onload = removeComment
+//     // visibility = "hidden"
+
+setTimeout(removeComment,1000)
+
+
